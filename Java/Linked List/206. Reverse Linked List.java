d2addd49cd254eb99c9cce395473cf733a072c9c
saahilanande <saahilanande21@gmail.com>
2023-07-03 18:13:13 -0500

    206. Reverse Linked List added in java

---
+++ Java/Linked List/206. Reverse Linked List.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode cur = head;
+        ListNode prev = null;
+        ListNode tmp = null;
+
+        while(cur != null){
+            tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+
+        return prev;
+
+        
+    }
+}
