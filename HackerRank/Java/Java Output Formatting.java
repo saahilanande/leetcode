03671d21f12222ca85be63e0b577bb7b4a0d7ae9
saahilanande <saahilanande21@gmail.com>
2023-07-19 20:43:30 -0500

    HackerRank output formatting

---
+++ HackerRank/Java/Java Output Formatting.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                String padded = "";
+                for(int j =s1.length(); j<15;j++){
+                    s1= s1+" ";
+                }
+                if(x<100){
+                    padded = "0";
+                }
+                if (x<10){
+                    padded = "00";
+                }
+                
+                System.out.println(s1+padded+x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
